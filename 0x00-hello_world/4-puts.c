056fd39f782d4b05b0cbf027e95766d3258f2e4e
VictorChidex <victorchidex1@gmail.com>
2022-12-08 17:03:32 -0800

    Fixed Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 /**
+ *  main - Entry point
  *
- *  * main - Entry point
+ *  Description: 'Prints the Programiming is like building a multilingual puzzle'
  *
- *   * 
- *
- *    * Description: 'Prints the Programiming is like building a multilingual puzzle'
- *
- *     * 
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ *  Return: Always 0 (Success)
+ */
 
 int main(void)
 {
