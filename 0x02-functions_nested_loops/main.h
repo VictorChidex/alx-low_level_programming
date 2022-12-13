3668aa2759b24f88667836b407e158df368b8619
VictorChidex <victorchidex1@gmail.com>
2022-12-13 13:41:59 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+/* Function to print _putchar*/
 int _putchar(char c);
+
+/**Write a function that prints the alphabet, in lowercase, followed by a new line.
+ * Prototype: void print_alphabet(void);
+ * You can only use _putchar twice in your code
+ */
+void print_alphabet(void);
 
 #endif
