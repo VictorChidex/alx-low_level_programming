1bfc7f421967d6c9d8f0e7de42509bd6b34b84d9
VictorChidex <victorchidex1@gmail.com>
2022-12-13 20:54:57 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - print n to 98
+ * print_below_98 - print n to 98
  * @n: int type defined in function
  * Retune: Always return 0
  */
@@ -19,11 +19,16 @@ void print_below_98(int n)
 	}
 }
 
+/**
+ * print_above_98 - check for numbers above 98
+ * @d: int type
+ * Return: return all values including 98
+ */
 void print_above_98(int d)
 {
 	int b;
 
-	for(b=d; b >= 98; b--)
+	for (b = d; b >= 98; b--)
 	{
 		if (b != 98)
 			printf("%d, ", b);
@@ -32,11 +37,15 @@ void print_above_98(int d)
 	}
 }
 
-
-void print_to_98(int n)
+/**
+ * print_to_98 - check for numbers below 98
+ * @a: int type
+ * Return: return all values including 98
+ */
+void print_to_98(int a)
 {
-	if (n <= 98)
-		print_below_98(n);
+	if (a <= 98)
+		print_below_98(a);
 	else
-	print_above_98(n);
+	print_above_98(a);
 }
