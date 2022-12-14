889b30f595c9aec580bfce86a1833de1cc6a14ed
VictorChidex <victorchidex1@gmail.com>
2022-12-13 20:37:34 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,7 @@
  * @n: int type defined in function
  * Retune: Always return 0
  */
-void print_to_98(int n)
+void print_below_98(int n)
 {
 	int c;
 
@@ -17,4 +17,26 @@ void print_to_98(int n)
 		else
 			printf("%d\n", c);
 	}
+}
+
+void print_above_98(int d)
+{
+	int b;
+
+	for(b=d; b >= 98; b--)
+	{
+		if (b != 98)
+			printf("%d, ", b);
+		else
+			printf("%d\n", b);
+	}
+}
+
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+		print_below_98(n);
+	else
+	print_above_98(n);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -37,7 +37,8 @@ int add(int, int);
 
 /*Write a function that prints all natural numbers from n to 98, followed by a new line. Prototype: void print_to_98(int n); Numbers must be separated by a comma, followed by a space Numbers should be printed in order The first printed number should be the number passed to your function The last printed number should be 98 You are allowed to use the standard library */
 void print_to_98(int n);
-
+void print_below_98(int a);
+void print_above_98(int d);
 
 
 #endif
