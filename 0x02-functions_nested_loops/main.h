e0c030690fa83ff200d77525e5b9718ac2f36c9a
VictorChidex <victorchidex1@gmail.com>
2022-12-13 19:08:18 -0800

    9. Learn your times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -27,4 +27,7 @@ int print_last_digit(int);
 /* Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59. Prototype: void jack_bauer(void);*/
 void jack_bauer(void);
 
+/*Write a function that prints the 9 times table, starting with 0. Prototype: void times_table(void); Format: see example*/
+void times_table(void);
+
 #endif
